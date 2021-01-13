6f515beca9969089a151bceee713a97cbbde9572
Michelle Hey <48541701+HeyMichelle@users.noreply.github.com>
2021-01-12 20:48:55 -0500

    feat: completed search function of a binary tree with notes

--- src/practice.py
+++ src/practice.py
@@ -7,7 +7,7 @@ class BSTNode:
     def insert(self, value):
         if value <= self.value:
             # the new value must go on the left
-            if self.left in None:
+            if self.left is None:
                 # create a new node as a left child of current node
                 self.left = BSTNode(value)
             else:
@@ -24,9 +24,58 @@ class BSTNode:
                 # let the right hand Node figure it out
                 self.right.insert(value)
     
+    # this will always return true or false if the item is not at the current node, what is if we need to search further? see next code, lol
     def search(self, target):
+        # the first thing to check when finding a value in the tree
+        # check against the root, check to see if target item even exists
+        if target == self.value:
+            # we have found the item
+            return True
+        if target < self.value:
+            # search the left side of the tree
+            # if we found nothing, then we know it's pointing to none
+            if self.left is None:
+                return False
+            return self.left.search(target) 
+            # if a node exists to the left, call search on that node. This allows us to search the next node
                 
+        else:
+            # Search the right side of the tree
+            if self.right is None:
+                return False
+            return self.right.search(target) # check to see if item is at the next node, it will return true 
+            # true is the value it will carry back and traverse back to the root with the returned-value. 
+            # Now we're back to the initial function call that started this whole thing with self is = 9 
+            # we push up calls and wait for them to give us the answer, true or false, passing it on to each function 
+        
+    def print_tree(self):
+        if self.left is not None:
+            self.left.print_tree()
 
+        print(self.value)
+
+        if self.right is not None:
+            self.right.print_tree()
+
+
+root = BSTNode(8)
+root.insert(5)
+root.insert(7)
+root.insert(12)
+root.insert(11)
+
+print(root.search(7))
+print(root.search(9))
+
+root.insert(9)
+print(root.search(9))
+print(root.print_tree()) # to print whole tree
+
+# what is the runtime of print? 
+    # how many nodes do you have to visit?
+    # linear O(n), you'll finish every single node
+
+# this differs from the SEARCH function O(log(n)) WOOT WOOT! because once found you can skip entire parts of your data structure (hopefully by half - if A balanced tree).
 
 # Binary Search Trees
     # all values on left must be smaller than the parent node
